d2b71c6b61b0e98eb8ae0b6d4bbb813673513c06
Anne Jacquet <anne-jacquet@sfr.fr>
2018-02-12 18:22:04 +0100

    add back and change dir

---
+++ Front/app/action/action.js
@@ -0,0 +1,24 @@
+app.service('Action', ['$http', function($http) {
+    let Action = function (data) {
+        this.name = data.name;
+        this.symbol = data.symbol;
+        this.priceBuy = data.priceBuy;
+        this.number = data.number;
+        this.priceActual = data.priceActual;
+    };
+    Action.prototype.buy = function(number) {
+        console.log("buy : ");
+        console.log(this);
+        console.log(number);
+        this.number = number;
+        $http.post('http://localhost:8082/wallet', this).then(function(response) {
+            console.log('ok');
+        }, function(error) { console.log(error); });
+    };
+    Action.prototype.sell = function(number) {
+        console.log("sell : ");
+        console.log(this);
+        console.log(number);
+    };
+    return Action;
+}]);
