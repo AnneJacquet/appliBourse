b999ff0aa1402dbf66754456ab5c5e8eba163725
Anne Jacquet <anne-jacquet@sfr.fr>
2018-03-22 20:48:48 +0100

    indent

--- Front/app/market/market_controller.js
+++ Front/app/market/market_controller.js
@@ -2,75 +2,75 @@ app.controller('MarketController',
     ['$scope', '$rootScope', '$mdDialog', '$http', '$timeout', 'Market', 'ActionMarket',
         function ($scope, $rootScope, $mdDialog, $http, $timeout, Market, ActionMarket) {
 
-        $scope.evol = "day";
+            $scope.evol = "day";
 
-        let inputChangedPromise;
+            let inputChangedPromise;
 
-        $scope.show = function (symbol) {
-            if (inputChangedPromise) {
-                $timeout.cancel(inputChangedPromise);
-            }
-            inputChangedPromise = $timeout(() => getMarket(symbol), 300);
-        };
-
-
-        function getMarket(symbol) {
-            Market = {actions: []};
-            if (symbol) {
-                $http.get('http://0.0.0.0:4000/market/' + symbol).then(function (response) {
-                    response.data.forEach(function (data) {
-                        let newAction = new ActionMarket(data);
-                        Market.actions.push(newAction);
-                    });
-                    $scope.market = Market;
-                }, function (error) {
-                    console.log(error);
-                });
-            } else {
-                $scope.market = Market;
-            }
-        }
-
-
-        //modal is open when click buy
-        $scope.confirmBuy = function (action, ev) {
-
-            $mdDialog.show({
-                controller: DialogController,
-                templateUrl: 'template/buyModal.html',
-                parent: angular.element(document.body),
-                targetEvent: ev,
-                clickOutsideToClose: true,
-                locals: {
-                    action: action
+            $scope.show = function (symbol) {
+                if (inputChangedPromise) {
+                    $timeout.cancel(inputChangedPromise);
                 }
-            })
-        };
-
-        //controller the opened modal
-        function DialogController($scope, $mdDialog, action) {
-
-            $scope.choosenNumber = 1;
-            $scope.numberToBuy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-            $scope.actionToBuy = action;
-
-            $scope.cancel = function () {
-                $mdDialog.cancel();
+                inputChangedPromise = $timeout(() => getMarket(symbol), 300);
             };
 
-            $scope.buy = function () {
-                console.log("buy : ", action, " - ", $scope.choosenNumber);
-                action.number = $scope.choosenNumber;
-                //close the modal
-                $mdDialog.hide();
-                //add the action to the wallet
-                $http.post('http://0.0.0.0:4000/wallet', action).then(function (response) {
-                    $rootScope.$broadcast('refreshWallet');
-                }, function (error) {
-                    console.log(error);
-                });
 
+            function getMarket(symbol) {
+                Market = {actions: []};
+                if (symbol) {
+                    $http.get('http://0.0.0.0:4000/market/' + symbol).then(function (response) {
+                        response.data.forEach(function (data) {
+                            let newAction = new ActionMarket(data);
+                            Market.actions.push(newAction);
+                        });
+                        $scope.market = Market;
+                    }, function (error) {
+                        console.log(error);
+                    });
+                } else {
+                    $scope.market = Market;
+                }
+            }
+
+
+            //modal is open when click buy
+            $scope.confirmBuy = function (action, ev) {
+
+                $mdDialog.show({
+                    controller: DialogController,
+                    templateUrl: 'template/buyModal.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true,
+                    locals: {
+                        action: action
+                    }
+                })
             };
-        }
 
-    }]);
+            //controller the opened modal
+            function DialogController($scope, $mdDialog, action) {
+
+                $scope.choosenNumber = 1;
+                $scope.numberToBuy = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+                $scope.actionToBuy = action;
+
+                $scope.cancel = function () {
+                    $mdDialog.cancel();
+                };
+
+                $scope.buy = function () {
+                    console.log("buy : ", action, " - ", $scope.choosenNumber);
+                    action.number = $scope.choosenNumber;
+                    //close the modal
+                    $mdDialog.hide();
+                    //add the action to the wallet
+                    $http.post('http://0.0.0.0:4000/wallet', action).then(function (response) {
+                        $rootScope.$broadcast('refreshWallet');
+                    }, function (error) {
+                        console.log(error);
+                    });
+
+                };
+            }
+
+        }]);
